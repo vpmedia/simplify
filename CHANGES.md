2368abf9b3efb4deef2ed0a228b8edd5a5540396
Andras Csizmadia <andras@vpmedia.hu>
2024-10-03 20:02:55 +0200

    Improved fetch retry implementation

--- CHANGES.md
+++ CHANGES.md
@@ -3,7 +3,7 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
-## 1.4.0
+## 1.5.0
 
 - Added fetch retry helper
 
--- src/util/fetchRetry.js
+++ src/util/fetchRetry.js
@@ -10,33 +10,28 @@ const logger = new Logger('fetchRetry');
  * @param {{ delay?: number, numRetries?: number, isLog?: boolean}} [retryOptions] - Retry options.
  * @returns {Promise<Response>} Fetch result.
  */
-export const fetchRetry = (resource, fetchOptions, retryOptions) => {
+export const fetchRetry = async (resource, fetchOptions, retryOptions) => {
   retryOptions = retryOptions ?? {};
   retryOptions.delay = retryOptions.delay ?? 1000;
   retryOptions.numRetries = retryOptions.numRetries ?? 3;
-  retryOptions.isLog = retryOptions.isLog ?? false;
-  return fetch(resource, fetchOptions)
-    .then((response) => {
-      if (retryOptions.isLog && !response.ok) {
-        logger.warn('Fetch failed', { fetchOptions, responseStatus: response.status });
+  while (retryOptions.numRetries > 0) {
+    logger.info('Fetch request', { resource, fetchOptions, retryOptions });
+    try {
+      const response = await fetch(resource, fetchOptions);
+      if (!response.ok || retryOptions.numRetries > 3) {
+        logger.warn('Fetch response', response);
+        throw new Error(`Network error: ${response.status}`, { cause: response.status });
       }
-      return response.ok ? response.json() : Promise.reject(response);
-    })
-    .catch((error) => {
-      if (retryOptions.isLog) {
-        logger.exception('Fetch error', error);
-      }
-      retryOptions.numRetries -= 1;
-      retryOptions.delay *= 2;
-      if (retryOptions.numRetries > 0) {
-        if (retryOptions.isLog) {
-          logger.warn('Fetch retry', { resource, fetchOptions, retryOptions });
-        }
-        delayPromise(retryOptions.delay).then(() => {
-          return fetchRetry(resource, fetchOptions, retryOptions);
-        });
-      } else {
+      logger.info('Fetch response', response);
+      return response;
+    } catch (error) {
+      logger.error('Fetch error', error instanceof Error ? error : new Error(String(error)));
+      if (retryOptions.numRetries === 0) {
         throw error;
       }
-    });
+      await delayPromise(retryOptions.delay);
+      retryOptions.numRetries -= 1;
+      retryOptions.delay *= 2;
+    }
+  }
 };
