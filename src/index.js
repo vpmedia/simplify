7835f05560238a8a8420aa2a4c443e4e87110aa9
Andras Csizmadia <andras@vpmedia.hu>
2024-10-06 14:01:59 +0200

    Added degree to radian conversion helper

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * from './const/http_status.js';
 export { Logger } from './logging/Logger.js';
 export { addLeadingZero } from './util/addLeadingZero.js';
 export { capitalize } from './util/capitalize.js';
+export { deg2rot } from './util/deg2rot.js';
 export { delayPromise } from './util/delayPromise.js';
 export { FetchError, fetchRetry } from './util/fetchRetry.js';
 export { getObjValueByPath } from './util/getObjValueByPath.js';
@@ -11,3 +12,4 @@ export { getURLParam } from './util/getURLParam.js';
 export { sanitizeURLParam } from './util/sanitizeURLParam.js';
 export { setObjValueByPath } from './util/setObjValueByPath.js';
 export { underscoreToCamelCase } from './util/underscoreToCamelCase.js';
+
---
+++ src/util/deg2rot.js
@@ -0,0 +1,8 @@
+/**
+ * Converts degrees to radians.
+ * @param {number} deg - Degree value.
+ * @returns {number} Radian value.
+ */
+export const deg2rot = (deg) => {
+  return deg * (Math.PI / 180);
+};
