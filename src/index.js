7d81b8733799bdcf0451dfbf77ef86e4fdd50124
Andras Csizmadia <andras@vpmedia.hu>
2024-10-06 14:08:17 +0200

    Added save as file helper

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ export { FetchError, fetchRetry } from './util/fetchRetry.js';
 export { getObjValueByPath } from './util/getObjValueByPath.js';
 export { getRandomInt } from './util/getRandomInt.js';
 export { getURLParam } from './util/getURLParam.js';
+export { purgeObject } from './util/purgeObject.js';
 export { sanitizeURLParam } from './util/sanitizeURLParam.js';
+export { saveAsFile } from './util/saveAsFile.js';
 export { setObjValueByPath } from './util/setObjValueByPath.js';
 export { underscoreToCamelCase } from './util/underscoreToCamelCase.js';
+
