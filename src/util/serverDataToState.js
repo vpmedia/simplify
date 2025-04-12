6796f3355781fe54eeb3e55fe9088fd874820503
Andras Csizmadia <andras@vpmedia.hu>
2025-04-12 12:12:23 +0200

    Added recursive flag to server data to state helper

--- src/util/serverDataToState.js
+++ src/util/serverDataToState.js
@@ -3,18 +3,19 @@ import { underscoreToCamelCase } from './underscoreToCamelCase.js';
 /**
  * Maps server data to client data.
  * @param {object} data - The server input data.
+ * @param {boolean} isRecursive - Use recursive serialization.
  * @returns {object} The output data.
  */
-export const serverDataToState = (data) => {
+export const serverDataToState = (data, isRecursive = false) => {
   if (Array.isArray(data)) {
-    return data.map((entry) => serverDataToState(entry));
+    return data.map((entry) => serverDataToState(entry, isRecursive));
   }
 
   if (data !== null && typeof data === 'object') {
     const result = {};
     for (const [key, value] of Object.entries(data)) {
       const clientKey = underscoreToCamelCase(key);
-      result[clientKey] = serverDataToState(value);
+      result[clientKey] = isRecursive ? serverDataToState(value) : value;
     }
     return result;
   }
